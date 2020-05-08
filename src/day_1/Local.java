4b01296852da63b1e197843b531092f0e9be33cf
andipepa <64115365+andipepa@users.noreply.github.com>
2020-05-08 15:35:55 -0400

    Update Local.java

--- src/day_1/Local.java
+++ src/day_1/Local.java
@@ -2,6 +2,8 @@ package day_1;
 
 public class Local {
     public static void main(String[] args) {
+        
+       //you have to work more on this
         ///users are able to call through slack
     }
 }
